f7a040f80734976299497087bb1690afd6494fcb
cm <18852676029@163.com>
2019-02-25 14:10:48 +0800

    'RemoveElement'

---
+++ RemoveElement/RemoveElement.js
@@ -0,0 +1,10 @@
+var removeElement = function(nums, val) {
+  var i = 0;
+  for(var j = 0;j<nums.length;j++){
+    if(nums[j] != val){
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return nums;
+};
